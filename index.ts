75c51b668cf6bd65eefccdc3654db368ba0a083b
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-02-12 19:47:15 +0530

    upd8

--- index.ts
+++ index.ts
@@ -158,13 +158,19 @@ activeuser = activeuser.filter((item) => item["id"] !== x);
 
 const io = new Server(server,{
     cors: {
-      origin: "https://projectylechat.vercel.app",
-      methods: ["GET", "POST"],
-      credentials: true,
+      origin: "*",
+      
     },
   });
-  
-// const io = new Server(server);
+
+// const io = new Server(server,{
+//     cors: {
+//       origin: "https://projectylechat.vercel.app",
+//       methods: ["GET", "POST"],
+//       credentials: true,
+//     },
+//   });
+
 
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
