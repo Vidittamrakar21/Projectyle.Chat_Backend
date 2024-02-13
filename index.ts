038efc9f91bfab270c328ff84401451ffe0c51c0
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-02-13 21:03:52 +0530

    up

--- index.ts
+++ index.ts
@@ -46,7 +46,7 @@ app.use(cors( {
 
  const io = new Server(server,{
     cors: {
-      origin: "https://projectylechat.vercel.app",
+      origin: "https://projectylechat.vercel.app/chat",
       methods: ["GET", "POST"],
       credentials: true,
     },
